85827fd3bf17248fcb7602750afdee04ea6cbee8
leosantosw <leonardosantosx0077@gmail.com>
2026-01-22 23:27:56 -0300

    feat: add credits to desktop sidebar

--- components/dashboard/sidebar/Sidebar.tsx
+++ components/dashboard/sidebar/Sidebar.tsx
@@ -1,9 +1,13 @@
 import SidebarNav from "./SidebarNav";
+import UserCredits from "./UserCredits";
 
 export default function Sidebar() {
   return (
     <aside className="hidden w-64 shrink-0 md:block">
       <div className="sticky top-24 rounded-2xl border border-border bg-card/80 p-5 shadow-sm">
+        <div className="pb-6">
+          <UserCredits />
+        </div>
         <SidebarNav />
       </div>
     </aside>
