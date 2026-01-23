ca4f3b34f032f9899e6a9d507d5a25fc0298ff22
leosantosw <leonardosantosx0077@gmail.com>
2026-01-22 23:27:43 -0300

    feat: show credits in dashboard layout

--- components/dashboard/DashboardLayout.tsx
+++ components/dashboard/DashboardLayout.tsx
@@ -1,10 +1,8 @@
-"use client";
-
-import { ReactNode, useState } from "react";
-import DashboardHeader from "./header/DashboardHeader";
+import { ReactNode } from "react";
 import DashboardFooter from "./footer/DashboardFooter";
-import MobileSidebar from "./sidebar/MobileSidebar";
 import Sidebar from "./sidebar/Sidebar";
+import DashboardNavigation from "./DashboardNavigation";
+import UserCredits from "./sidebar/UserCredits";
 
 type DashboardLayoutProps = {
   children: ReactNode;
@@ -16,24 +14,15 @@ export default function DashboardLayout({
   children,
   title,
 }: DashboardLayoutProps) {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
   return (
     <div className="min-h-screen bg-background text-text">
-      <DashboardHeader
-        title={title}
-        onMenuOpen={() => setIsSidebarOpen(true)}
-      />
+      <DashboardNavigation title={title} mobileCredits={<UserCredits />} />
       <div className="mx-auto w-full max-w-6xl px-6 py-10">
         <div className="flex gap-8">
           <Sidebar />
           <main className="min-w-0 flex-1">{children}</main>
         </div>
       </div>
-      <MobileSidebar
-        isOpen={isSidebarOpen}
-        onClose={() => setIsSidebarOpen(false)}
-      />
       <DashboardFooter />
     </div>
   );
